22b80d6938dca1f98a85d1ea03f91cd3b2a36cdb
tomoyayoshino <t.yoshino0331@gmail.com>
2020-07-12 18:22:16 +0900

    ログイン機能を実装

--- src/main.js
+++ src/main.js
@@ -3,8 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdt1dQxrUk1XqR8j6ALOZMdiTcMpPbZDA",
+  authDomain: "my-address-pj-d5931.firebaseapp.com",
+  databaseURL: "https://my-address-pj-d5931.firebaseio.com",
+  projectId: "my-address-pj-d5931",
+  storageBucket: "my-address-pj-d5931.appspot.com",
+  messagingSenderId: "628163984301",
+  appId: "1:628163984301:web:8ed99270dc856b587fff24",
+  measurementId: "G-0QRNN6VV86"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 
 new Vue({
   router,
